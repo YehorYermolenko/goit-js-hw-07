b987f19e64877c13e2247fc72ccb3fac49a853f9
YehorYermolenko <egorermolenko17@gmail.com>
2021-03-06 00:39:18 +0200

    корректировки от ментора....

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
 const ulCategories = document.querySelectorAll(".item");
 console.log(`В списке ${ulCategories.length} категории.`);
-
 const ulLiHeadings = ulCategories.forEach((el) => {
-  console.log(`Категория: ${el.querySelector("h2").textContent}`);
-  console.log(`Количество элементов: ${el.children[1].children.length}`);
+  console.log(`Категория: ${el.firstElementChild.textContent}`);
+  console.log(`Количество элементов: ${el.lastElementChild.children.length}`);
 });
