6e04594a242241f2b94650699ab35ddb3853cce1
YehorYermolenko <egorermolenko17@gmail.com>
2021-03-07 00:29:32 +0200

    Задание 2 готово

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,13 @@
 const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
+
+const newIngredient = document.querySelector(`#ingredients`);
+
+const newElements = ingredients.map((ingredient) => {
+  const newLi = document.createElement("li");
+  newLi.textContent = ingredient;
+  return newLi;
+});
+
+console.log(newElements);
+
+newIngredient.append(...newElements);
