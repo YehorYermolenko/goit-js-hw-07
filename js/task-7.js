614ea457e1ba7116574dd9e60d984cbf7aad29f5
YehorYermolenko <egorermolenko17@gmail.com>
2021-03-07 22:56:52 +0200

    Заканчивает 7е задание

--- js/task-7.js
+++ js/task-7.js
@@ -1,1 +1,8 @@
 const fontInput = document.querySelector("#font-size-control");
+const spanText = document.querySelector("#text");
+
+const inputChange = (elem) => {
+  spanText.style.fontSize = `${elem.target.value}px`;
+};
+
+fontInput.addEventListener("input", inputChange);
